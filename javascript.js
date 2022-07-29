291ef988e2fe1019123b942cb27414fcfc707724
Abbass_Jradi <jradiabbass@gmail.com>
2022-07-29 11:34:02 +0300

    Add the computer choice generating function

--- javascript.js
+++ javascript.js
@@ -3,5 +3,8 @@ var player_choice = '0';
 
 choices = ['Rock', 'Paper', 'Scissor'];
 
-
+function getComputerChoice(choices){
+    index = Math.floor(Math.random()*choices.length);
+    return choices[index];
+}
 
