e2d4194772295f458b1a323a0e4a9102a2c08419
Abbass_Jradi <jradiabbass@gmail.com>
2022-07-29 12:48:21 +0300

    Add the single round function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
-var computer_choice = '0';
-var player_choice = '0';
+let computer_choice = '0';
+let player_choice = '0';
+let winner = '0';
 
 choices = ['Rock', 'Paper', 'Scissor'];
 
@@ -8,7 +9,42 @@ function getComputerChoice(choices){
     return choices[index];
 }
 
+function singleRound(player_selection, computer_selection){
+    player_selection = player_selection.toLowerCase();
+    computer_selection = computer_selection.toLowerCase();
+
+    if ((player_selection === 'paper') && (computer_selection === 'rock')){
+      winner = player_selection;
+      return "You Win ! Paper Beats Rock";
+    }
+    else if(player_selection === 'rock' && computer_selection === 'paper'){
+      winner = computer_selection;
+      return "You Lose ! Paper Beats Rock";
+    }
+
+    if (player_selection === 'scissor' && computer_selection === 'paper'){
+      winner = player_selection;
+      return "You Win ! Scissor Beats Paper";
+      }
+      else if(player_selection === 'paper' && computer_selection === 'scissor'){
+      winner = computer_selection;
+      return "You Lose ! Scissor Beats Paper";
+      }
+    
+    if (player_selection === 'rock' && computer_selection === 'scissor'){
+      winner = player_selection;
+      return "You Win ! Rock Beats Scissor";
+    }
+    else if(player_selection === 'scissor' && computer_selection === 'rock'){
+      winner = computer_selection;
+      return "You Lose ! Rock Beats Scissor";
+    }
+
+    if (player_selection === computer_selection) {
+        return 'Draw';
+    }
+}
+
 user_choice = prompt("Enter your choice: ");
-
-
-
+computer_choice = getComputerChoice(choices);
+console.log(singleRound(user_choice, computer_choice));
